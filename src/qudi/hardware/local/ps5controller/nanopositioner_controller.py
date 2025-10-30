da8cae7896f953d6b98b050b504879c4a7e9253a
ywpi4 <yijun.wang@physics.uni-hannover.de>
2025-10-30 14:38:27 +0100

    ps5controller_event7

--- src/qudi/hardware/local/ps5controller/nanopositioner_controller.py
+++ src/qudi/hardware/local/ps5controller/nanopositioner_controller.py
@@ -241,10 +241,10 @@ class pos_controller:
 if __name__ == '__main__':
     while(True):
         try:
-            try:
-                gamepad = InputDevice('/dev/input/event8')
-            except FileNotFoundError:
-                gamepad = InputDevice('/dev/input/event4')
+            # try:
+            gamepad = InputDevice('/dev/input/event7')
+            # except FileNotFoundError:
+            #     gamepad = InputDevice('/dev/input/event4')
 
             while True:
                 r, w, x = select.select([gamepad],[],[])
